06119e14803a9a6bdd805e07e1c06458ccea6d60
Maxnxi <nisq@ya.ru>
2022-07-11 14:55:00 +0300

    Initial Commit

---
+++ avtodocTest/avtodocTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  avtodocTestApp.swift
+//  avtodocTest
+//
+//  Created by Maksim Ponomarev on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct avtodocTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
